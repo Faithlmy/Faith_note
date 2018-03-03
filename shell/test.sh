191c712d5c21af000c82e62b32074409297ee1ef
Faithlmy <lmengyy@126.com>
2018-03-03 21:25:22 +0800

    shell

--- shell/test.sh
+++ shell/test.sh
@@ -1,7 +1,14 @@
- !#/bin/bash
- url = http://www.google.com
- echo $url
- name = 'name'
- echo $name
- author = "中介"
- echo $author
+#!/bin/bash
+data
+who
+echo UID:$UID
+echo HOME:$HOME
+#变量
+day=10
+meng=pp
+gest="faith"
+my='kit'
+echo "this is $day and $gest or $my , in  $meng"
+
+#命令替换
+
